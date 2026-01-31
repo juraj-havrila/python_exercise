3a0e98f84a6d86cc6c19df2cad6a4c95a1e37251
juraj-havrila <34303416+juraj-havrila@users.noreply.github.com>
2026-01-31 13:57:55 +0100

    Refactor game loop for improved performance
    Adjusted game speed and added food collision logic.

--- sneke-game/main.py
+++ sneke-game/main.py
@@ -1,6 +1,11 @@
-from  turtle import Screen, Turtle
 import time
+from  turtle import Screen
+
+from scoreboard import Scoreboard
 from snake import Snake
+from food import Food
+from scoreboard import Scoreboard
+
 
 screen = Screen()
 screen.setup(width=600, height=600)
@@ -9,6 +14,8 @@ screen.title("Snake Game")
 screen.tracer(0)
 
 snake = Snake()
+food = Food()
+score = Scoreboard()
 
 screen.listen()
 screen.onkey(snake.up,"Up")
@@ -19,9 +26,22 @@ screen.onkey(snake.right, "Right")
 game_is_on = True
 while game_is_on:
     screen.update()
-    time.sleep(1)
+    time.sleep(0.2)
     snake.move()
+    if snake.head.distance(food) < 15:
+        food.refresh()
+        score.increase_score()
+        snake.extend()
 
-#for i in range(snake_length):
+    if snake.head.xcor()>280 or snake.head.xcor()<-280 or snake.head.ycor()<-280 or snake.head.ycor()>280:
+        game_is_on = False
+        score.game_over()
 
+    for segment in snake.segments[1:-1]:
+        #if segment == snake.head:
+        #   pass
+        if snake.head.distance(segment) < 15:
+            game_is_on = False
+            score.game_over()
+            
 screen.exitonclick()
