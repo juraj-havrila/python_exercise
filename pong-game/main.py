d3de17194646af7f49cc2209f68684eb41fc5767
juraj-havrila <34303416+juraj-havrila@users.noreply.github.com>
2026-02-06 19:16:18 +0100

    Refactor game loop and add paddle and ball initialization

--- pong-game/main.py
+++ pong-game/main.py
@@ -1,4 +1,8 @@
 from turtle import Screen
+from paddle import Paddle
+from ball import Ball
+from scoreboard import Scoreboard
+import time
 screen = Screen()
 
 screen.setup(width=800, height=600)
@@ -6,11 +10,36 @@ screen.bgcolor("black")
 screen.title("Pong Game")
 screen.tracer(0)
 
+r_paddle=Paddle((350,0))
+l_paddle=Paddle((-350,0))
+ball = Ball()
+scoreboard = Scoreboard()
+
+screen.listen()
+screen.onkey(r_paddle.go_up,"Up")
+screen.onkey(r_paddle.go_down,"Down")
+screen.onkey(l_paddle.go_up,"w")
+screen.onkey(l_paddle.go_down,"s")
+
 game_is_on = True
-
 while game_is_on:
-
-    screen.listen()
+    time.sleep(ball.move_speed)
     screen.update()
-    
+    ball.move()
+
+    if ball.ycor() > 290 or ball.ycor() < -290:
+        ball.bounce_y()
+
+    if ball.distance(r_paddle) < 50 and ball.xcor() > 340 or ball.distance(l_paddle) < 50 and ball.xcor() < -340:
+        ball.bounce_x()
+    if ball.xcor() > 400:
+        ball.reset_position()
+        scoreboard.l_point()
+        #ball.bounce_x()
+    if ball.xcor() < -400:
+        ball.reset_position()
+        #ball.bounce_x()
+        scoreboard.r_point()
+
 screen.exitonclick()
+
