d6dabaea67400d7914b60df692a00c1d4ab96eb5
juraj-havrila <34303416+juraj-havrila@users.noreply.github.com>
2026-02-08 13:14:31 +0100

    Add high score functionality to scoreboard

--- sneke-game/scoreboard.py
+++ sneke-game/scoreboard.py
@@ -6,6 +6,7 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
+        self.highscore = 0
         self.penup()
         self.hideturtle()
         self.color("white")
@@ -13,14 +14,25 @@ class Scoreboard(Turtle):
         self.speed("fastest")
         self.update_scoreboard()
 
-    def update_scoreboard(self):
-        self.write(f"Score: {self.score}", align=ALIGNMENT, font=FONT)
 
-    def game_over(self):
-        self.goto(0, 0)
-        self.write(f"GAME OVER!", align=ALIGNMENT, font=FONT)
+    def update_scoreboard(self):
+        self.clear()
+        with open ("data.txt", mode="r") as file:
+            self.highscore = int(file.read())
+        self.write(f"Score: {self.score}  High Score: {self.highscore}", align=ALIGNMENT, font=FONT)
+
+    #def game_over(self):
+    #    self.goto(0, 0)
+    #    self.write(f"GAME OVER!", align=ALIGNMENT, font=FONT)
+
+    def reset(self):
+        if self.score > self.highscore:
+            self.highscore = self.score
+            with open("data.txt", mode="w") as file:
+                file.write(str(self.highscore))
+        self.score = 0
+        self.update_scoreboard()
 
     def increase_score(self):
         self.score += 1
-        self.clear()
         self.update_scoreboard()
