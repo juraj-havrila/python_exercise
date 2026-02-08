82cbb87fb0c586daac7a87ab192bb979924ec264
juraj-havrila <34303416+juraj-havrila@users.noreply.github.com>
2026-02-08 13:13:37 +0100

    Disable game over conditions and reset on collision
    Comment out game over logic and reset functionality.

--- sneke-game/main.py
+++ sneke-game/main.py
@@ -34,14 +34,18 @@ while game_is_on:
         snake.extend()
 
     if snake.head.xcor()>280 or snake.head.xcor()<-280 or snake.head.ycor()<-280 or snake.head.ycor()>280:
-        game_is_on = False
-        score.game_over()
+        #game_is_on = False
+        #score.game_over()
+        score.reset()
+        snake.reset()
 
     for segment in snake.segments[1:-1]:
         #if segment == snake.head:
         #   pass
         if snake.head.distance(segment) < 15:
-            game_is_on = False
-            score.game_over()
-            
+            #game_is_on = False
+            #score.game_over()
+            score.reset()
+            snake.reset()
+
 screen.exitonclick()
