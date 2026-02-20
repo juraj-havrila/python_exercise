8f9df1f720c0d4cd446b6f023978d5f2deb709c7
juraj-havrila <34303416+juraj-havrila@users.noreply.github.com>
2026-02-20 19:23:57 +0100

    Implement validation for user input letters
    Add check for valid letters in NATO alphabet.

--- NATO-Alphabet-SpellScript.py
+++ NATO-Alphabet-SpellScript.py
@@ -3,4 +3,7 @@ data=pandas.read_csv("nato_phonetic_alphabet.csv")
 NATO_Alphabet={row.letter:row.code for (index, row) in data.iterrows()}
 user_input=list((input("Please enter your text which you want to have spelled out: ")).upper())
 for letter in user_input:
-    print(f"{letter} -- {NATO_Alphabet[letter]}")
+    if letter in NATO_Alphabet.keys():
+        print(f"{letter} -- {NATO_Alphabet[letter]}")
+    else:
+        print("")
