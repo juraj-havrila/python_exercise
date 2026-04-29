02bf3900cb4ccf4896438aa14a94f0361dcd6501
juraj-havrila <34303416+juraj-havrila@users.noreply.github.com>
2026-04-29 06:34:07 +0200

    Add weather forecast retrieval functionality
    Fetch weather forecast data from OpenWeatherMap API.

---
+++ weather_forecast.py
@@ -0,0 +1,23 @@
+import requests
+OWM_ENDPOINT= "https://api.openweathermap.org/data/2.5/forecast"
+api_key = "xxx"
+LATITUDE = "x"
+LONGITUDE = "x"
+
+weather_params = {
+    "lat" : LATITUDE,
+    "lon" : LONGITUDE,
+    "units" : "metric",
+    "appid" : api_key,
+    "lang" : "en",
+    "cnt" : 4,              #count of 3-hour intervals for forecast (4x3 = 12 hours)
+    }
+
+
+
+
+response = requests.get(OWM_ENDPOINT, params=weather_params)
+response.raise_for_status()
+weather_data = response.json()
+
+
